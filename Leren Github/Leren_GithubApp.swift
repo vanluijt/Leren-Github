01068f406befcd66531671666cf94f3f3be3385a
vanluijt <vl54@ziggo.nl>
2022-08-05 11:01:55 +0200

    Initial Commit

---
+++ Leren Github/Leren_GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Leren_GithubApp.swift
+//  Leren Github
+//
+//  Created by P.A. van Luijt on 05/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Leren_GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
